0a1316742087944afc427a5d7cee23c3a2282492
ywl <ywl172632955@gmail.com>
2016-05-09 19:18:43 +0800

    no message

---
+++ LoveHouse/LoveHouse/ChoseCropView.m
@@ -0,0 +1,21 @@
+//
+//  ChoseCropView.m
+//  LoveHouse
+//
+//  Created by ywl on 16/5/9.
+//  Copyright © 2016年 wanli.yang. All rights reserved.
+//
+
+#import "ChoseCropView.h"
+
+@implementation ChoseCropView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
